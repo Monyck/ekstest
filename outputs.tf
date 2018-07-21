e3e3dec4ba59b750dc7e0e6e2d257cc9cb14e004
root <root@ip-172-31-63-120.ec2.internal>
2018-07-21 14:40:05 +0000

    fixed outputs

--- outputs.tf
+++ outputs.tf
@@ -58,6 +58,10 @@ output "kubeconfig" {
   value = "${local.kubeconfig}"
 }
 
+output "endpoint" {
+  value = "${aws_eks_cluster.demo.endpoint}"
+}
+
 output "kubeconfig-certificate-authority-data" {
   value = "${aws_eks_cluster.demo.certificate_authority.0.data}"
 }
